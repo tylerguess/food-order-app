3370be4ac88b6275306e84ed22bc735e05a1d414
Tyler Guess <johntylerguess@gmail.com>
2021-06-08 22:30:45 -0500

    cart context and cart state

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
-import classes from "./HeaderCartButton.module.css"
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberCartItems = cartCtx.items.reduce((curVal, item) => {
+    return curVal + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberCartItems}</span>
     </button>
   );
 };
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,7 +1,23 @@
+import { useRef, useState } from 'react';
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNum = +enteredAmount;
+    if (enteredAmount.trim().length === 0 || enteredAmountNum < 1 || enteredAmountNum > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNum);
+
+  }
   const inputConfig = {
     id: "amount_" + props.id,
     type: "number",
@@ -11,9 +27,10 @@ const MealItemForm = (props) => {
     defaultValue: "1",
   };
   return (
-    <form className={classes.form}>
-      <Input label="Amount" input={inputConfig} />
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input ref={amountInputRef} label="Amount" input={inputConfig} />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1 - 5)</p>}
     </form>
   );
 };
